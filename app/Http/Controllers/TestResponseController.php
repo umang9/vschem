204004bbd31bdbb969ce8fdc9d32eeaa18853671
monish.c <monish.c@media.net>
2018-08-31 04:30:59 +0530

    Support for logging; Log submit test SP calls

--- app/Http/Controllers/TestResponseController.php
+++ app/Http/Controllers/TestResponseController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 include_once '../ud-utils/ApiUtil.php';
+include_once '../ud-utils/LoggerUtil.php';
 include_once '../ud-constants/database.php';
 
 class TestResponseController extends Controller
@@ -27,6 +28,8 @@ class TestResponseController extends Controller
         }
         $responseXml .= "</r></rt>";
         $spCall = 'call ' . \SPCalls::TEST_SUBMIT_API . '(@xml:="' . $responseXml .'",@user_id:=' . $request->user()->id . ',@test_id:=' . $test_id .  ',@start_time:="' . $request->start_time . '",@end_time:="' . $request->end_time . '")';
+
+        \LoggerUtil::logDataToFile($_SERVER ['DOCUMENT_ROOT'] .'../logs/spcall', $spCall);
         $dbResponse = json_decode(json_encode(DB::select($spCall)),true);
         $output['success'] = true;
         $output['data'] = $dbResponse;
