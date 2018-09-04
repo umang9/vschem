6271e9528dd03f645a054cfa6bfb5a5de65e8476
Sushant Jadhav <work.sushantjadhav@gmail.com>
2018-09-04 19:57:33 +0530

    all.css and all.js files are added

--- resources/views/welcome-react.blade.php
+++ resources/views/welcome-react.blade.php
@@ -6,15 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel React application</title>
+    {{--This is use for react file--}}
     <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
     <link rel="manifest" href="{{asset("mix-manifest.json")}}">
     <link rel="shortcut icon" href="{{asset("favicon.ico")}}">
-    <link rel="stylesheet" href="{{asset("assets/plugins/bootstrap/css/bootstrap.min.css")}}">
-    <link href="{{asset("assets/plugins/morrisjs/morris.css")}}" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{mix("css/all.css")}}">
+    {{--<link rel="stylesheet" href="{{asset("assets/plugins/bootstrap/css/bootstrap.min.css")}}">--}}
+    {{--<link href="{{asset("assets/plugins/morrisjs/morris.css")}}" rel="stylesheet">--}}
     <link href="{{asset("css/style.css")}}" rel="stylesheet">
     <link href="{{asset("css/colors/default-dark.css")}}" id="theme" rel="stylesheet">
-
-    <link href="{{asset("assets/plugins/horizontal-timeline/css/horizontal-timeline.css")}}" rel="stylesheet">
+    {{--<link href="{{asset("assets/plugins/horizontal-timeline/css/horizontal-timeline.css")}}" rel="stylesheet">--}}
 </head>
 <body class="fix-header fix-sidebar card-no-border">
 <noscript>
@@ -30,21 +32,24 @@
 </div>
 
     <script src="{{mix('js/app.js')}}"></script>
-    <script src="{{asset("assets/plugins/jquery/jquery.min.js")}}"></script>
-    <script src="{{asset("assets/plugins/bootstrap/js/popper.min.js")}}"></script>
-    <script src="{{asset("assets/plugins/bootstrap/js/bootstrap.min.js")}}"></script>
-    <script src="{{asset("js/jquery.slimscroll.js")}}"></script>
-    <script src="{{asset("js/waves.js")}}"></script>
-    <script src="{{asset("js/sidebarmenu.js")}}"></script>
-    <script src="{{asset("assets/plugins/sticky-kit-master/dist/sticky-kit.min.js")}}"></script>
-    <script src="{{asset("js/custom.min.js")}}"></script>
-    <script src="{{asset("assets/plugins/sparkline/jquery.sparkline.min.js")}}"></script>
-    <script src="{{asset("assets/plugins/raphael/raphael-min.js")}}"></script>
-    <script src="{{asset("assets/plugins/morrisjs/morris.min.js")}}"></script>
-    <script src="{{asset("js/dashboard.js")}}"></script>
-    <script src="{{asset("assets/plugins/wizard/jquery.steps.min.js")}}"></script>
-    <script src="{{asset("assets/plugins/wizard/jquery.validate.min.js")}}"></script>
-    <script src="{{asset("assets/plugins/horizontal-timeline/js/horizontal-timeline.js")}}"></script>
-    <script src="{{asset("assets/plugins/styleswitcher/jQuery.style.switcher.js")}}"></script>
+    <script src="{{mix('js/all.js')}}"></script>
+
+    {{--<script src="{{asset("assets/plugins/jquery/jquery.min.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/bootstrap/js/popper.min.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/bootstrap/js/bootstrap.min.js")}}"></script>--}}
+    {{--<script src="{{asset("js/jquery.slimscroll.js")}}"></script>--}}
+    {{--<script src="{{asset("js/waves.js")}}"></script>--}}
+    {{--<script src="{{asset("js/sidebarmenu.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/sticky-kit-master/dist/sticky-kit.min.js")}}"></script>--}}
+    {{--<script src="{{asset("js/custom.min.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/sparkline/jquery.sparkline.min.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/raphael/raphael-min.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/morrisjs/morris.min.js")}}"></script>--}}
+    {{--<script src="{{asset("js/dashboard.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/wizard/jquery.steps.min.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/wizard/jquery.validate.min.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/horizontal-timeline/js/horizontal-timeline.js")}}"></script>--}}
+    {{--<script src="{{asset("assets/plugins/styleswitcher/jQuery.style.switcher.js")}}"></script>--}}
+
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,28 @@ let mix = require('laravel-mix');
 
 mix.react('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.scripts([
+    './public/assets/plugins/jquery/jquery.min.js',
+    './public/assets/plugins/bootstrap/js/popper.min.js',
+    './public/assets/plugins/bootstrap/js/bootstrap.min.js',
+    './public/js/jquery.slimscroll.js',
+    './public/js/waves.js',
+    './public/js/sidebarmenu.js',
+    './public/assets/plugins/sticky-kit-master/dist/sticky-kit.min.js',
+    './public/js/custom.min.js',
+    './public/assets/plugins/sparkline/jquery.sparkline.min.js',
+    './public/assets/plugins/raphael/raphael-min.js',
+    './public/assets/plugins/morrisjs/morris.min.js',
+    './public/js/dashboard.js',
+    './public/assets/plugins/wizard/jquery.steps.min.js',
+    './public/assets/plugins/wizard/jquery.validate.min.js',
+    './public/assets/plugins/horizontal-timeline/js/horizontal-timeline.js',
+    './public/assets/plugins/styleswitcher/jQuery.style.switcher.js',
+], 'public/js/all.js');
+
+mix.styles([
+    './public/assets/plugins/bootstrap/css/bootstrap.min.css',
+    './public/assets/plugins/morrisjs/morris.css',
+    './public/assets/plugins/horizontal-timeline/css/horizontal-timeline.css',
+], 'public/css/all.css');
